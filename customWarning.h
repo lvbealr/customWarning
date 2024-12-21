56bbf002e7c03be325cf86e59f13d9443e4de304
lvbealr <lvbealr@mail.ru>
2024-12-21 03:33:26 +0300

    add customAssert and colorful print

--- customWarning.h
+++ customWarning.h
@@ -3,12 +3,25 @@
 
 #include <cstdio>
 
-#define customWarning(expression, returnMessage) do {                      \
-    if (!(expression)) {                                                   \
-        printf("Assertion Error: %s, file (%s), function (%s), line %d\n", \
-                #expression, __FILE__, __FUNCTION__, __LINE__);            \
-        return returnMessage;                                              \
-    }                                                                      \
+#include "colorPrint.h"
+
+#define customWarning(expression, returnMessage) do {                         \
+    if (!(expression)) {                                                      \
+        customPrint(yellow, bold, bgDefault, "[WARNING]: %s ", #expression);  \
+        customPrint(white,  bold, bgDefault, "// %s:%d (%s)", __FILE__,       \
+                                                __LINE__, __FUNCTION__);      \
+        return returnMessage;                                                 \
+    }                                                                         \
+} while (0)
+
+#define customAssert(expression, returnMessage) do {                               \
+    if (!(expression)) {                                                           \
+        customPrint(red, bold, bgDefault, "[ASSERTION ERROR]: %s ", #expression);  \
+        customPrint(white,  bold, bgDefault, "// %s:%d (%s) => ", __FILE__,        \
+                                                    __LINE__, __FUNCTION__);       \
+        customPrint(red, bold, bgDefault, "EXIT...\n");                            \
+        exit(returnMessage);                                                       \
+    }                                                                              \
 } while (0)
 
 #endif // CUSTOM_WARNING_H_
