794bd213cc984b4298c542af8891ec83a8fe8429
lvbealr <lvbealr@mail.ru>
2025-04-17 17:24:33 +0300

    conditional compilation for macros

--- customWarning.h
+++ customWarning.h
@@ -5,23 +5,28 @@
 
 #include "colorPrint.h"
 
-#define customWarning(expression, returnMessage) do {                            \
-    if (!(expression)) {                                                         \
-        customPrint(yellow, bold, bgDefault, "[WARNING]: %s ", #returnMessage);  \
-        customPrint(white,  bold, bgDefault, "// %s:%d (%s)", __FILE__,          \
-                                                __LINE__, __FUNCTION__);         \
-        return returnMessage;                                                    \
-    }                                                                            \
-} while (0)
+#ifndef NDEBUG
+    #define customWarning(expression, returnMessage) do {                            \
+        if (!(expression)) {                                                         \
+            customPrint(yellow, bold, bgDefault, "[WARNING]: %s ", #returnMessage);  \
+            customPrint(white,  bold, bgDefault, "// %s:%d (%s)\n", __FILE__,          \
+                                                    __LINE__, __FUNCTION__);         \
+            return returnMessage;                                                    \
+        }                                                                            \
+    } while (0)
 
-#define customAssert(expression, returnMessage) do {                                  \
-    if (!(expression)) {                                                              \
-        customPrint(red, bold, bgDefault, "[ASSERTION ERROR]: %s ", #returnMessage);  \
-        customPrint(white,  bold, bgDefault, "// %s:%d (%s) => ", __FILE__,           \
-                                                    __LINE__, __FUNCTION__);          \
-        customPrint(red, bold, bgDefault, "EXIT...\n");                               \
-        exit(returnMessage);                                                          \
-    }                                                                                 \
-} while (0)
+    #define customAssert(expression, returnMessage) do {                                  \
+        if (!(expression)) {                                                              \
+            customPrint(red, bold, bgDefault, "[ASSERTION ERROR]: %s ", #returnMessage);  \
+            customPrint(white,  bold, bgDefault, "// %s:%d (%s) => ", __FILE__,           \
+                                                        __LINE__, __FUNCTION__);          \
+            customPrint(red, bold, bgDefault, "EXIT...\n");                               \
+            exit(returnMessage);                                                          \
+        }                                                                                 \
+    } while (0)
+#else
+    #define customWarning
+    #define customAssert
+#endif
 
 #endif // CUSTOM_WARNING_H_
